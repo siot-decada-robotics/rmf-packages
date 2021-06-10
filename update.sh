569bbfe4dd967a0f0c0d054c0e0e938c2211b23a
pj <hpj@u.nus.edu>
2021-06-11 02:23:14 +0800

    update submodules to the latest commit

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+switch_main()
+{
+    cd $1
+    for f in *; do cd $f; git switch main; cd ..; done;
+    cd ..
+}
+
+switch_main demonstrations
+switch_main rmf 
+cd thirdparty/menge_vendor; git switch master;
+cd ../ros_ign; git switch melodic; cd ../..
+git add .; git commit -m "update submodules to the latest commit"; git push
